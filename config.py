fec67eb6ee6a38db725d2ae018ca059de7e9d46c
Numonov Sardorbek <numonovsardorbek@gmail.com>
2020-05-18 14:12:43 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-host = "https://api-88CA8C74-5A5F-4A67-ABCA-C23D24C33C37.sendbird.com/v3"
-bot_id = "info_bot"
-api_token = "3e4bb5be554433a29fb8cced3a38b74aae39f85f"
+host = "https://api-<Replace with your APP ID>.sendbird.com/v3"
+bot_id = "<Replace with your bot id"
+api_token = "<Replace with your API Token>"
